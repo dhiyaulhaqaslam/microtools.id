39b679fb1057250e9e881530c76e989708a72119
dhiyaulhaqaslam <dhias313@gmail.com>
2026-09-22 18:25:01 +0800

    fix: simpel UI/UX

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './enhancements.css'
 import './templates.css'
+import './premium-business.css'
+import './action-system.css'
+import './item-actions.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
